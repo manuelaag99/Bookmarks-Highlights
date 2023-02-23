682dc929670e5d8cf8cabe12fa1555034b7de855
Manuel Alanis <manuelaag99@gmail.com>
2023-02-22 22:12:38 -0600

    20230222 tags functionality with commas

--- bookmarks-and-highlights-frontend/src/Components/BodyForAddOrUpdate.jsx
+++ bookmarks-and-highlights-frontend/src/Components/BodyForAddOrUpdate.jsx
@@ -2,16 +2,14 @@ import React from "react";
 
 import { addOrUpdateFormReducer } from "../Reducers";
 import FormForAddOrUpdate from "./FormForAddOrUpdate";
-import { nonEmptyText } from "../CheckValidity";
 import PhotoForAddOrUpdate from "./PhotoForAddOrUpdate";
 import TagsSection from "./TagsSection";
 
 const BodyForAddOrUpdate = props => {
-    console.log(props ? "yeah" : "no")
     // this function establishes the inputs and their validity status
     const [stateOfForm, dispatch] = React.useReducer(addOrUpdateFormReducer, {
         inputs: {
-            title: { value: "", isValid: props.isAdd ? false : true},
+            title: { value: "", isValid: false},
             date: { value: "", isValid: false },
             page: { value: "", isValid: false },
             tags: { value: "", isValid: false }
@@ -25,20 +23,30 @@ const BodyForAddOrUpdate = props => {
     }, [dispatch])
 
     // this function checks, with each change to the form, whether it's valid or not, and sends it to the parent component
-    const sendValidity = props => {
+    const changeHandle = props => {
         props.isFormValid(stateOfForm)
         console.log(stateOfForm)
+    }
+    
+    const initialTagsState = props.isAdd ? [] : props.itemValues.tags
+    const [ tagsState, setTagsState ] = React.useState(initialTagsState)
+
+    const keyHandle = e => {
+        if (e.key === ",") {
+            console.log("comma")
+            if (tagsState !== stateOfForm.inputs.tags.value) {
+                setTagsState(() => stateOfForm.inputs.tags.value)
+            }
+        }
     }
 
     const submitHandler = e => {
         e.preventDefault()
         console.log(stateOfForm)
     }
-
-    const initialTagsState = props.isAdd ? null : props.itemValues.tags
     
     return (
-        <form id="add-or-update-form" onChange={() => {sendValidity(props)}} onSubmit={submitHandler} className="w-85 md:h-6/10 h-8/10 md:mt-32 mt-16 flex flex-wrap flex-row">
+        <form id="add-or-update-form" onChange={() => {changeHandle(props)}} onKeyDown={keyHandle} onSubmit={submitHandler} className="w-85 md:h-6/10 h-8/10 md:mt-32 mt-16 flex flex-wrap flex-row">
             <PhotoForAddOrUpdate photo="" />
             <div className="w-3/5 md:h-7/10 h-3/10 pl-6 block md:hidden">
                 <FormForAddOrUpdate isAddPage={props.isAdd} onInput={inputHandler} field="title" initialValue={props.isAdd ? null : props.itemValues.bookTitle} errorText="error!" labelText="Title of the book/article:" placeholderText="i.e. Title (author, year)" inputType="text" />
@@ -48,13 +56,12 @@ const BodyForAddOrUpdate = props => {
                     <FormForAddOrUpdate isAddPage={props.isAdd} onInput={inputHandler} field="title" initialValue={props.isAdd ? null : props.itemValues.bookTitle || null} errorText="error!" labelText="Title of the book/article:" placeholderText="i.e. Title (author, year)" inputType="text" />
                 </div>
                 <div className="flex flex-wrap flex-row justify-start">
-                    <FormForAddOrUpdate isAddPage={props.isAdd} onInput={inputHandler} field="date" initialValue={props.isAdd ? null : props.itemValues.date || null} errorText="error!" labelText="date:" placeholderText="DD/MM/YYYY" classnames="w-2/3 " inputType="date" value="30/09/2020" />
+                    <FormForAddOrUpdate isAddPage={props.isAdd} onInput={inputHandler} field="date" initialValue={props.isAdd ? null : props.itemValues.date || null} errorText="error!" labelText="date:" placeholderText="DD/MM/YYYY" classnames="w-2/3 " inputType="date" />
                     <FormForAddOrUpdate isAddPage={props.isAdd} onInput={inputHandler} field="page" initialValue={props.isAdd ? null : props.itemValues.pageNumber || null} errorText="error!" labelText="page #:" placeholderText="e.g. 31, 105" classnames="w-3/10 " inputType="number" />
                 </div>
                 <FormForAddOrUpdate isAddPage={props.isAdd} onInput={inputHandler} field="tags" initialValue={props.isAdd ? null : initialTagsState.toString().replaceAll(",", ", ") || null} errorText="error!" labelText="tags:" placeholderText="e.g. geography..." classnames="w-full mt-6 " />
-                
                 <div className="mt-6">
-                    {props.isAdd ? null : <TagsSection tagsArray={initialTagsState} />}
+                    <TagsSection tagsArray={tagsState} />
                 </div>
             </div>
         </form>
--- bookmarks-and-highlights-frontend/src/MOCKDATA.js
+++ bookmarks-and-highlights-frontend/src/MOCKDATA.js
@@ -29,40 +29,40 @@ export { users }
 
 const entries = [
     {
-        userId: "0001",
-        bookTitle: "The History of Latin America (Wilson, 2008)",
-        bookId: "684087503948",
-        photoUrl: "http://static.everypixel.com/ep-pixabay/1694/0943/3745/51620/16940943374551620074-book.jpg",
-        tags: [
-          "history", "latin america", "colonization"
-        ],
-        date: "2022/11/05",
-        pageNumber: "30",
-        itemId: "7585749430"
+      userId: "0001",
+      bookTitle: "The History of Latin America (Wilson, 2008)",
+      bookId: "684087503948",
+      photoUrl: "http://static.everypixel.com/ep-pixabay/1694/0943/3745/51620/16940943374551620074-book.jpg",
+      tags: [
+        "history", "latin america", "colonization"
+      ],
+      date: "11/05/2022",
+      pageNumber: "30",
+      itemId: "7585749430"
     },
     {
-        userId: "0001",
-        bookTitle: "The History of Latin America (Wilson, 2008)",
-        bookId: "684087503948",
-        photoUrl: "https://media.istockphoto.com/id/470986879/photo/book-page-love.jpg?s=612x612&w=0&k=20&c=i1500WHaaoSu250PoQI9jJnf_xtMy_g7Q_qT7nHfbns=",
-        tags: [
-          "history", "latin america", "colonization", "slavery"
-        ],
-        date: "2022/11/05",
-        pageNumber: "35",
-        itemId: "3548354536"
+      userId: "0001",
+      bookTitle: "The History of Latin America (Wilson, 2008)",
+      bookId: "684087503948",
+      photoUrl: "https://media.istockphoto.com/id/470986879/photo/book-page-love.jpg?s=612x612&w=0&k=20&c=i1500WHaaoSu250PoQI9jJnf_xtMy_g7Q_qT7nHfbns=",
+      tags: [
+        "history", "latin america", "colonization", "slavery"
+      ],
+      date: "11/05/2022",
+      pageNumber: "35",
+      itemId: "3548354536"
     },
     {
-        userId: "0001",
-        bookTitle: "The History of Latin America (Wilson, 2008)",
-        bookId: "684087503948",
-        photoUrl: "https://preview.redd.it/5p1njt4sjdx11.jpg?auto=webp&s=e915268a22edc7d8fda95ca143d8543f0cbe498c",
-        tags: [
-          "history", "latin america", "colonization", "labor", "population", "family", "violence", "racism", "political economy", "segregation", "colonialism", "mexico", "indigenous people"
-        ],
-        date: "2022/11/30",
-        pageNumber: "65",
-        itemId: "119742953112"
+      userId: "0001",
+      bookTitle: "The History of Latin America (Wilson, 2008)",
+      bookId: "684087503948",
+      photoUrl: "https://preview.redd.it/5p1njt4sjdx11.jpg?auto=webp&s=e915268a22edc7d8fda95ca143d8543f0cbe498c",
+      tags: [
+        "history", "latin america", "colonization", "labor", "population", "family", "violence", "racism", "political economy", "segregation", "colonialism", "mexico", "indigenous people"
+      ],
+      date: "11/30/2022",
+      pageNumber: "65",
+      itemId: "119742953112"
     },
     {
       userId: "0001",
@@ -72,81 +72,81 @@ const entries = [
       tags: [
         "history", "latin america", "colonization", "population", "family"
       ],
-      date: "2022/11/30",
+      date: "11/30/2022",
       pageNumber: "67",
       itemId: "6132514241311"
-  },
-    {
-        userId: "0001",
-        bookTitle: "The History of Europe (Jones, 2020)",
-        bookId: "23434343243543",
-        photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
-        tags: [
-          "history", "europe", "liberalism", "colonization"
-        ],
-        date: "2021/10/10",
-        pageNumber: "100",
-        itemId: "07896957632"
     },
     {
-        userId: "0001",
-        bookTitle: "The History of Europe (Jones, 2020)",
-        bookId: "23434343243543",
-        photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
-        tags: [
-          "history", "discrimination", "conspiracy", "antisemitism"
-        ],
-        date: "2022/20/10",
-        pageNumber: "205",
-        itemId: "578336284721"
+      userId: "0001",
+      bookTitle: "The History of Europe (Jones, 2020)",
+      bookId: "23434343243543",
+      photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
+      tags: [
+        "history", "europe", "liberalism", "colonization"
+      ],
+      date: "10/10/2021",
+      pageNumber: "100",
+      itemId: "07896957632"
     },
     {
-        userId: "0001",
-        bookTitle: "Origins of Antisemitism (Kyle, 2000)",
-        bookId: "898392847787",
-        photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
-        tags: [
-          "history", "europe", "liberalism", "colonization"
-        ],
-        date: "2021/10/10",
-        pageNumber: "100",
-        itemId: "434652343635"
+      userId: "0001",
+      bookTitle: "The History of Europe (Jones, 2020)",
+      bookId: "23434343243543",
+      photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
+      tags: [
+        "history", "discrimination", "conspiracy", "antisemitism"
+      ],
+      date: "10/20/2022",
+      pageNumber: "205",
+      itemId: "578336284721"
     },
     {
-        userId: "0001",
-        bookTitle: "Origins of Antisemitism (Kyle, 2000)",
-        bookId: "898392847787",
-        photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
-        tags: [
-          "history", "discrimination", "conspiracy", "antisemitism"
-        ],
-        date: "2022/20/10",
-        pageNumber: "205",
-        itemId: "797121325352"
+      userId: "0001",
+      bookTitle: "Origins of Antisemitism (Kyle, 2000)",
+      bookId: "898392847787",
+      photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
+      tags: [
+        "history", "europe", "liberalism", "colonization"
+      ],
+      date: "10/10/2021",
+      pageNumber: "100",
+      itemId: "434652343635"
     },
     {
-        userId: "0002",
-        bookTitle: "The History of Europe (Jones, 2020)",
-        bookId: "684087503948",
-        photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
-        tags: [
-          "history", "discrimination", "conspiracy", "antisemitism"
-        ],
-        date: "2022/20/10",
-        pageNumber: "205",
-        itemId: "233452343523"
+      userId: "0001",
+      bookTitle: "Origins of Antisemitism (Kyle, 2000)",
+      bookId: "898392847787",
+      photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
+      tags: [
+        "history", "discrimination", "conspiracy", "antisemitism"
+      ],
+      date: "10/20/2022",
+      pageNumber: "205",
+      itemId: "797121325352"
     },
     {
-        userId: "0002",
-        bookTitle: "The History of Europe (Jones, 2020)",
-        bookId: "684087503948",
-        photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
-        tags: [
-          "history", "europe", "liberalism", "colonization"
-        ],
-        date: "2021/10/10",
-        pageNumber: "100",
-        itemId: "95847758485321"
+      userId: "0002",
+      bookTitle: "The History of Europe (Jones, 2020)",
+      bookId: "684087503948",
+      photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
+      tags: [
+        "history", "discrimination", "conspiracy", "antisemitism"
+      ],
+      date: "10/20/2022",
+      pageNumber: "205",
+      itemId: "233452343523"
+    },
+    {
+      userId: "0002",
+      bookTitle: "The History of Europe (Jones, 2020)",
+      bookId: "684087503948",
+      photoUrl: "https://preview.redd.it/d9766td2pnzy.jpg?auto=webp&s=4c68eb76c689f8b1db0c7a8d816ae3eb22959822",
+      tags: [
+        "history", "europe", "liberalism", "colonization"
+      ],
+      date: "10/10/2021",
+      pageNumber: "100",
+      itemId: "95847758485321"
     }
 
 ]
