058886569407d0a47d09550449da962fdb875dc2
Manuel Alanis <manuelaag99@gmail.com>
2023-02-28 12:22:09 -0600

    failed different functionality

--- bookmarks-and-highlights-frontend/src/Components/Authentication.jsx
+++ bookmarks-and-highlights-frontend/src/Components/Authentication.jsx
@@ -6,12 +6,20 @@ import InputsAuthAndButton from "./InputsAuthAndButton";
 import OAuthAndGoBack from "./OAuthAndGoBack";
 
 const Authentication = props => {
+    const [ formValidity, setformValidity ] = React.useState(false)
+    const checkFormValidity = (stateOfForm) => {
+        setformValidity(() => stateOfForm.isValid)
+    }
+
     return (
         <div className="md:w-1/2 w:full my-12 h-screen">
             <div className="flex flex-col justify-center items-center">
                 <div className="flex flex-wrap xl:w-1/2 w-7/10 mx-auto justify-center">
                     <Breaker breakerText={props.upperText} isSignInPage={true}/>
-                    <InputsAuthAndButton buttonInput={props.authBtn} initialInputs={props.initialInputsForForm} usernamePlaceholder={props.usernamePlaceholder} emailPlaceholder={props.emailPlaceholder} passwordPlaceholder={props.passwordPlaceholder} confirmPasswordPlaceholder={props.confirmPasswordPlaceholder} type={props.upperText}/>
+                    <InputsAuthAndButton buttonInput={props.authBtn} isFormValid={checkFormValidity} initialInputs={props.initialInputsForForm} usernamePlaceholder={props.usernamePlaceholder} emailPlaceholder={props.emailPlaceholder} passwordPlaceholder={props.passwordPlaceholder} confirmPasswordPlaceholder={props.confirmPasswordPlaceholder} type={props.upperText}/>
+                    
+                    <Button buttonText={props.authBtn} classnames=" text-var-1 bg-var-4 hover:bg-var-4-hovered " form="sign-in-or-sign-up-form" isAbled={formValidity} isSignInOrSignUpButton={true} type="submit"  />
+                    
                     <Breaker breakerText="or" isSignInPage={true}/>
                     <OAuthAndGoBack googleOAuthText={props.googleBtn} facebookOAuthText={props.facebookBtn}/>
                 </div>
--- bookmarks-and-highlights-frontend/src/Components/InputsAuthAndButton.jsx
+++ bookmarks-and-highlights-frontend/src/Components/InputsAuthAndButton.jsx
@@ -10,28 +10,32 @@ const InputsAuthAndButton = props => {
   
     const [inputButtonValidity, setInputButtonValidity] = React.useState(false)
     const buttonValidity = React.useRef(false)
-    const changeHandler = () => {
-        console.log(stateOfAuthInputForm)
-        buttonValidity.current = stateOfAuthInputForm.isValid
-        // setInputButtonValidity(() => stateOfAuthInputForm.isValid)
-    }
+    // const changeHandler = () => {
+    //     console.log(stateOfAuthInputForm)
+    //     buttonValidity.current = stateOfAuthInputForm.isValid
+    //     // setInputButtonValidity(() => stateOfAuthInputForm.isValid)
+    // }
     console.log(buttonValidity.current)
     const submitHandler = e => {
         e.preventDefault()
         console.log(stateOfAuthInputForm)
     }
 
+    const changeHandle = props => {
+        props.isFormValid(stateOfAuthInputForm)
+    }
+
     return (
-        <form onChange={changeHandler} className="flex flex-wrap justify-center rounded-tag bg-var-2 w-full shadow-card my-5" id="sign-in-or-sign-up-form" onSubmit={submitHandler} >
+        <form onChange={() => changeHandle(props)} className="flex flex-wrap justify-center rounded-tag bg-var-2 w-full shadow-card my-5" id="sign-in-or-sign-up-form" onSubmit={submitHandler} >
             <div className="my-5 w-full flex flex-wrap justify-center">
                 <InputForAuth inputType="text" field="username" onInput={authInputHandler} placeholderText={props.usernamePlaceholder} />
                 {props.type === "Sign up" && <InputForAuth inputType="email" field="email" onInput={authInputHandler} placeholderText={props.emailPlaceholder} />}
                 <InputForAuth inputType="password" field="password" onInput={authInputHandler} placeholderText={props.passwordPlaceholder} />
                 {props.type === "Sign up" && <InputForAuth inputType="password" field="confirmPassword" onInput={authInputHandler} placeholderText={props.confirmPasswordPlaceholder} />}
             </div>
-            <Link className="w-9/10 mt-[-20px] mb-5" to={"/" + "0001" + "/myprofile"}>
+            {/* <Link className="w-9/10 mt-[-20px] mb-5" to={"/" + "0001" + "/myprofile"}>
                 <Button buttonText={props.buttonInput} classnames=" text-var-1 bg-var-4 hover:bg-var-4-hovered " form="sign-in-or-sign-up-form" isAbled={buttonValidity.current} isSignInOrSignUpButton={true} type="submit"  />
-            </Link>
+            </Link> */}
         </form>
     )
 }
