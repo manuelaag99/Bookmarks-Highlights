c91215f0d608a02029e695332da6c7bc2749e504
Manuel Alanis <manuelaag99@gmail.com>
2023-03-13 21:54:32 -0600

    correct deleted file

--- bookmarks-and-highlights-frontend/src/Components/Portals/Loading.jsx
+++ bookmarks-and-highlights-frontend/src/Components/Portals/Loading.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createPortal } from "react-dom"
 
 export default function Loading ({ open }) {
-
     const loading = (
         <div>
             <div className="bg-var-2 fixed top-0 bottom-0 w-screen h-screen flex justify-center items-center">
