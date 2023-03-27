484b8c593e1c044a4e002c2a710d3b3041b8c985
Manuel Alanis <manuelaag99@gmail.com>
2023-03-27 12:33:45 -0600

    functioning display of book titles

--- bookmarks-and-highlights-frontend/src/Components/FormWithListForAddOrUpdate.jsx
+++ bookmarks-and-highlights-frontend/src/Components/FormWithListForAddOrUpdate.jsx
@@ -3,8 +3,10 @@ import React, { useEffect, useRef, useState } from "react";
 import { useInput } from "../custom-hooks";
 
 export default function FormWithListForAddOrUpdate({ classnames, errorText, field, initialValidity, initialValue, inputType, isBookListOpen, labelText, listOfBooks, onInput, placeholderText, selectTitle, shouldBookListClose, shouldBookListOpen, valueFromList }) {
-    const DUMMYOPTIONS = [{ bookTitle: "The History of Europe", bookId: "85jt95rh4897h5948rj3i" },
-    { bookTitle: "The History of Europe", bookId: "903ekj24d2f6f42a0l000" }];
+    const DUMMYOPTIONS = [
+        { bookTitle: "The History of Europe", bookId: "85jt95rh4897h5948rj3i" },
+        { bookTitle: "The History of Europe", bookId: "903ekj24d2f6f42a0l000" }
+    ];
 
     // console.log(valueFromList)
     const [inputState, inputChangeHandler, inputBlurHandler, chooseFromListHandler] = useInput({ value: initialValue, isValid: initialValidity });
@@ -15,7 +17,7 @@ export default function FormWithListForAddOrUpdate({ classnames, errorText, fiel
     const [listQuery, setListQuery] = useState([]);
     useEffect(() => { setListQuery(DUMMYOPTIONS) }, []);
 
-    
+    // const listToDisplay 
 
     const focusInHandler = () => shouldBookListOpen();
     const focusOutHandler = () => shouldBookListClose();
@@ -34,17 +36,27 @@ export default function FormWithListForAddOrUpdate({ classnames, errorText, fiel
         setDynamicValue(value);
     }, [value]);
 
-
+    let newList = DUMMYOPTIONS;
+    const [filteredList, setFilteredList] = useState(newList);
+    const filterList = () => {
+        newList = DUMMYOPTIONS.filter(option => {
+            return option.bookTitle.toLowerCase().includes(dynamicValue)
+        });
+        setFilteredList(newList);
+    };
+    useEffect(() => filterList(), [dynamicValue]);
+    
 
     return (
         <div className={"h-16 mt-3 " + classnames}>
             <label className={"inline text-add-or-update-p font-bold " + ((!inputState.isValid && inputState.isTouched) ? "text-red-btn" : null)}>{labelText}</label>
             <i className={"ml-3 text-red-btn " + ((!inputState.isValid && inputState.isTouched) ? "inline" : "hidden")}>{errorText}</i>
-            <input className="block outline-none mt-1 pl-2 h-8 w-95 pr-2" type={inputType} placeholder={placeholderText} value={dynamicValue} onChange={inputChangeHandler} onFocus={focusInHandler} onBlur={inputBlurHandler} />
+            <input className="block outline-none mt-1 pl-2 h-8 w-95 pr-2" type={inputType} placeholder={placeholderText} value={dynamicValue} onFocus={focusInHandler} onBlur={inputBlurHandler} onChange={inputChangeHandler} />
             {isBookListOpen && <div className="absolute bg-var-1 text-var-5 w-95 h-fit cursor-pointer shadow-card">
-                {listQuery && DUMMYOPTIONS.map((book, index) => <p key={index} onClick={clickHandle} className="hover:bg-var-4 hover:text-var-1 duration-100 w-full px-3 py-2 h-fit place-self-center">{book.bookTitle}</p>)}
-                <p onClick={clickHandle} className="hover:bg-var-4 hover:text-var-1 duration-100 px-3 py-2 h-fit items-center">Add... </p>
+                {listQuery && filteredList.map((book, index) => <p key={index} onClick={clickHandle} className="hover:bg-var-4 hover:text-var-1 duration-100 w-full px-3 py-2 h-fit place-self-center">{book.bookTitle}</p>)}
+                <input className="outline-none py-2 px-3 h-fit items-center w-8/10" onChange={filterList} value={dynamicValue} />
+                <button className="w-1/10 h-full justify-center items-center px-4 py-2 hover:bg-var-4 hover:text-var-1">Add</button>
             </div>}
         </div>
-    )
+    );
 };
