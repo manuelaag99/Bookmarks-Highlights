c16fa8a1c02ba599c919462cccfbf7ce33d8fab4
Manuel Alanis <manuelaag99@gmail.com>
2023-03-27 10:54:15 -0600

    functioning display value for book title

--- bookmarks-and-highlights-frontend/src/Components/FormWithListForAddOrUpdate.jsx
+++ bookmarks-and-highlights-frontend/src/Components/FormWithListForAddOrUpdate.jsx
@@ -13,11 +13,12 @@ export default function FormWithListForAddOrUpdate({ classnames, errorText, fiel
     useEffect(() => onInput(field, value, isValid), [onInput, field, value, isValid])
 
     const [listQuery, setListQuery] = useState([]);
-    useEffect(() => { setListQuery(DUMMYOPTIONS) }, [DUMMYOPTIONS]);
+    useEffect(() => { setListQuery(DUMMYOPTIONS) }, []);
+
+    
 
     const focusInHandler = () => shouldBookListOpen();
     const focusOutHandler = () => shouldBookListClose();
-
     const clickHandle = e => {
         const listValue = e.target.innerText;+
         selectTitle(listValue);
@@ -25,25 +26,25 @@ export default function FormWithListForAddOrUpdate({ classnames, errorText, fiel
     };
 
     const [dynamicValue, setDynamicValue] = useState("");
-
     useEffect(() => {
         chooseFromListHandler(valueFromList);
         setDynamicValue(valueFromList);
     }, [valueFromList]);
-
     useEffect(() => {
         setDynamicValue(value);
     }, [value]);
+
+
 
     return (
         <div className={"h-16 mt-3 " + classnames}>
             <label className={"inline text-add-or-update-p font-bold " + ((!inputState.isValid && inputState.isTouched) ? "text-red-btn" : null)}>{labelText}</label>
             <i className={"ml-3 text-red-btn " + ((!inputState.isValid && inputState.isTouched) ? "inline" : "hidden")}>{errorText}</i>
             <input className="block outline-none mt-1 pl-2 h-8 w-95 pr-2" type={inputType} placeholder={placeholderText} value={dynamicValue} onChange={inputChangeHandler} onFocus={focusInHandler} onBlur={inputBlurHandler} />
-            {isBookListOpen && <list className="absolute bg-var-1 text-var-5 w-95 h-fit cursor-pointer shadow-card">
-                {listQuery && DUMMYOPTIONS.map(book => <p onClick={clickHandle} className="hover:bg-var-4 hover:text-var-1 duration-100 w-full px-3 h-8 place-self-center">{book.bookTitle}</p>)}
-                <p onClick={clickHandle} className="hover:bg-var-4 hover:text-var-1 duration-100 h-8 px-3 items-center">Add... </p>
-            </list>}
+            {isBookListOpen && <div className="absolute bg-var-1 text-var-5 w-95 h-fit cursor-pointer shadow-card">
+                {listQuery && DUMMYOPTIONS.map((book, index) => <p key={index} onClick={clickHandle} className="hover:bg-var-4 hover:text-var-1 duration-100 w-full px-3 py-2 h-fit place-self-center">{book.bookTitle}</p>)}
+                <p onClick={clickHandle} className="hover:bg-var-4 hover:text-var-1 duration-100 px-3 py-2 h-fit items-center">Add... </p>
+            </div>}
         </div>
     )
 };
