6d2c9a083fc77f667a363cab16d9ca6409dff5ce
Manuel Alanis <manuelaag99@gmail.com>
2023-04-16 15:47:03 -0600

    add change and delete functionality to add/update bandhs

--- bookmarks-and-highlights-frontend/src/Components/ImageUpload.jsx
+++ bookmarks-and-highlights-frontend/src/Components/ImageUpload.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 
-import { useInput } from "../custom-hooks";
-
 export default function ImageUpload ({ field, initialValidity, initialValue, onInput }) {
 
     const fileSelectorRef = useRef();
     const [file, setFile] = useState();
     const [previewUrl, setPreviewUrl] = useState();
     const [validity, setValidity] = useState(false);
-    const [isHovering, setIsHovering] = useState(false);
 
     useEffect(() => {
         if (!file) return;
@@ -39,22 +36,17 @@ export default function ImageUpload ({ field, initialValidity, initialValue, onI
         }
         onInput(field, selectedFile, fileIsValid);
     };
-
-    const onMouseOverHandler = () => {
-        console.log("hover");
-    };
-
-    const onMouseLeaveHandler = () => {
-        console.log("left");
-    };
     
     return (
-        <div className="flex flex-wrap justify-center text-center items-center cursor-pointer h-full max-h-[200px] bg-var-8" onClick={selectFileHandler} >
+        <div className="flex flex-wrap justify-center text-center items-center cursor-pointer h-full max-h-[200px] bg-var-8 relative z-0" onClick={previewUrl ? null : selectFileHandler} >
+            {previewUrl && <div className="absolute top-0 right-0 flex flex-row text-var-1 z-20">
+                <button className="rounded-tag bg-var-5 px-2 m-2" onClick={selectFileHandler} type="button">change</button>
+                <button className="rounded-tag bg-var-5 px-2 m-2" onClick={cancelFileUpload} type="button">X</button>
+            </div>}
             <input accept=".jpg,.png,.jpeg" className="w-full h-full " id={field} onChange={uploadFileHandler} ref={fileSelectorRef} style={{display: "none"}} type="file" />
-            <div className="w-full h-full flex flex-col justify-center items-center">
+            <div className="w-full h-full flex flex-col justify-center items-center relative">
                 {!previewUrl && <p className="w-full h-fit cursor-pointer">Select an image</p>}
-                {previewUrl && <img alt="Preview" className="h-full object-contain" onClick={selectFileHandler} onMouseOver={onMouseOverHandler} onMouseLeave={onMouseLeaveHandler} src={previewUrl} />}
-                {previewUrl && <button onClick={cancelFileUpload} type="button">CANCEL</button>}
+                {previewUrl && <img alt="Preview" className="h-full object-contain z-10" src={previewUrl} />}
             </div>
         </div>
     )
