512dfb56733ff50f398961170532d324fa327618
Manuel Alanis <manuelaag99@gmail.com>
2023-06-25 14:21:26 -0600

    front end profile picture fitting

--- bookmarks-and-highlights-frontend/src/Components/ProfilePic.jsx
+++ bookmarks-and-highlights-frontend/src/Components/ProfilePic.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ProfilePicture ({ photoSource }) {
     return (
         <div className="h-24 w-24 md:m-0 mt-6 mx-auto">
-            <img className="h-full object-cover rounded-circle" src={photoSource} alt="profile-photo" />
+            <img className="h-full w-full object-cover rounded-circle " src={photoSource} alt="profile-photo" />
         </div>
     )
 }
