9cad8c46baa69fb6a01d6e02d9cc05f1acda9626
Manuel Alanís <manuelaag99@gmail.com>
2024-02-23 15:12:40 -0600

    functional generic profile picture

--- bookmarks-and-highlights-frontend/src/Components/ProfilePic.jsx
+++ bookmarks-and-highlights-frontend/src/Components/ProfilePic.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import GenericProfilePicture from "../images/Generic-Profile.png"
 
 export default function ProfilePicture ({ photoSource }) {
     return (
         <div className="h-24 w-24 md:m-0 mt-6 mx-auto">
-            <img className="h-full w-full object-cover rounded-circle " src={"http://localhost:3000/" + photoSource} alt="profile-photo" />
+            {photoSource && <img className="h-full w-full object-cover rounded-circle " src={"http://localhost:3000/" + photoSource} alt="profile-photo" />}
+            {!photoSource && <img className="h-full w-full object-cover rounded-circle " src={GenericProfilePicture} alt="profile-photo" />}
         </div>
     )
 }
